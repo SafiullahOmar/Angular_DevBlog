9b222f164d8a1e805f053322a4baec7d3dd17a1c
Safiullah Omar <omar.safiullah@gmail.com>
2022-07-16 10:04:43 +0430

    API methods & DTO

---
+++ Blog/Controllers/PostsController.cs
@@ -0,0 +1,106 @@
+﻿using Blog.Data;
+using Blog.Models.DTO;
+using Blog.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly DevBlogDbContext dbContext;
+        public PostsController(DevBlogDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> getAllPosts() {
+            var posts =await dbContext.Posts.ToListAsync();
+            return Ok(posts);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetPostById")]
+        public async Task<IActionResult> GetPostById(Guid id) {
+            var post=await dbContext.Posts.FirstOrDefaultAsync(x => x.Id == id);
+            if (post != null) {
+                return Ok(post);
+            }
+
+            return NotFound();
+         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(AddPostRequest addPostRequest) {
+            var post = new Post() {
+            Title=addPostRequest.Title,
+            Content=addPostRequest.Content,
+            Author=addPostRequest.Author,
+            FeautredImageURL=addPostRequest.FeautredImageURL,
+            PublishDate=addPostRequest.PublishDate,
+            UpdateDate=addPostRequest.UpdateDate,
+            Summary=addPostRequest.Summary  ,
+            URLHandle=addPostRequest.URLHandle,
+            Visible=addPostRequest.Visible
+            };
+
+            post.Id = Guid.NewGuid();
+            await dbContext.Posts.AddAsync(post);
+            await dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetPostById), new { id = post.Id },post );
+         }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdatePost([FromRoute]Guid id,UpdatePostRequest updatePostRequest) {
+           
+
+            var existingPost = await dbContext.Posts.FindAsync(id);
+            if (existingPost != null) {
+                existingPost.Title = updatePostRequest.Title;
+                existingPost.Content = updatePostRequest.Content;
+                existingPost.Author = updatePostRequest.Author;
+                existingPost.FeautredImageURL = updatePostRequest.FeautredImageURL;
+                existingPost.PublishDate = updatePostRequest.PublishDate;
+                existingPost.UpdateDate = updatePostRequest.UpdateDate;
+                existingPost.Summary = updatePostRequest.Summary;
+                existingPost.URLHandle = updatePostRequest.URLHandle;
+                existingPost.Visible = updatePostRequest.Visible;
+
+                await dbContext.SaveChangesAsync();
+                return Ok(existingPost);
+            }
+
+            return NotFound();
+
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeletePost([FromRoute] Guid id)
+        {
+
+
+            var existingPost = await dbContext.Posts.FindAsync(id);
+            if (existingPost != null)
+            {
+
+                 dbContext.Posts.Remove(existingPost);
+                await dbContext.SaveChangesAsync();
+                return Ok(existingPost);
+            }
+
+            return NotFound();
+
+        }
+    }
+}
---
+++ Blog/Models/DTO/UpdatePostRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.DTO
+{
+    public class UpdatePostRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Summary { get; set; }
+        public string URLHandle { get; set; }
+        public string FeautredImageURL { get; set; }
+        public bool Visible { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
